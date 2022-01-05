4a7d864ba1d129dcb1a3a6f5b15dbec890afdb9f
liza-kl <hello@li54.dev>
2022-01-05 21:52:30 +0100

    :card_file_box: Wrote Database Connector for App

---
+++ src/main/kotlin/dev/li54/data/db/DatabaseConnector.kt
@@ -0,0 +1,21 @@
+package dev.li54.data.db
+import org.jetbrains.exposed.exceptions.ExposedSQLException
+import org.jetbrains.exposed.sql.*
+import io.github.cdimascio.dotenv.dotenv
+
+class DatabaseConnector {
+    private val dotenv = dotenv()
+    fun connectToMusicSocietyDB() {
+        try {
+            Database.connect(
+                "jdbc:postgresql://${dotenv["DB_HOST"]}:${dotenv["DB_PORT"]}/${dotenv["DB_DATABASE"]}",
+                driver = "org.postgresql.Driver",
+                user = dotenv["DB_USER"],
+                password = dotenv["DB_PASSWORD"]
+            )
+            println("Connection to Database was successful ")
+        } catch (ex : ExposedSQLException) {
+            println("Something went wrong with the Database Connection $ex")
+        }
+    }
+}
