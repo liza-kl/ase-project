079ff7d7a3c9721a97bfd4f3d8a5c4fd7fcb928d
liza-kl <hello@li54.dev>
2022-02-14 22:36:33 +0100

    :sparkles: Introduced a HOCON file for server configuration

--- plugins/src/main/kotlin/de/dhbw/ka/Application.kt
+++ plugins/src/main/kotlin/de/dhbw/ka/Application.kt
@@ -3,8 +3,11 @@ package de.dhbw.ka
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.application.*
 
-fun main() {
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing : Boolean = false) {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
