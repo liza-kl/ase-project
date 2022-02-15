7ec654973a30d2d550e9a8f007d07ccf90971091
liza-kl <hello@li54.dev>
2022-02-15 14:04:12 +0100

    :white_check_mark: made the code easier

--- domain/src/main/kotlin/de/dhbw/ka/domain/valueobjects/InstrumentAmbitus.kt
+++ domain/src/main/kotlin/de/dhbw/ka/domain/valueobjects/InstrumentAmbitus.kt
@@ -2,31 +2,30 @@ package de.dhbw.ka.domain.valueobjects
 
 import kotlin.math.absoluteValue
 
-
-fun generateTuneMap() : HashMap<Char, Int> {
-    val tuneMap : HashMap<Char, Int> = HashMap<Char, Int> ();
-    val keys = listOf<String>("C","D","E","F","G","A","H","c","d","e","f","g","a","h");
+fun generateTuneMap() : HashMap<String, Int> {
+    val tuneMap : HashMap<String, Int> = HashMap<String, Int> ();
+    val keys = listOf<String>(
+                            "C","D","E","F","G","A","H",
+                            "c","d","e","f","g","a","h",
+                            "c1","d1","e1","f1","g1","a1","h1",
+                            "c2","d2","e2","f2","g2","a2","h2",
+                            "c3","d3","e3","f3","g3","a3","h3",
+                            "c4","d4","e4","f4","g4","a4","h4");
     for ((i, key) in keys.withIndex()) {
-        tuneMap[key.first()] = i;
+        tuneMap[key] = i;
     }
     return tuneMap;
 }
 
 fun checkFormalities(firstTone: String, lastTone: String) : Boolean {
-    val possibleTonePattern = "(^([a-hA-H])[1-4]?){1,2}".toRegex();
-
-    if(!firstTone.matches(possibleTonePattern) || !lastTone.matches(possibleTonePattern)) {
-        throw Exception("The tone you've provided does not exist (yet)")
+    if(!generateTuneMap().containsKey(firstTone) || !generateTuneMap().containsKey(lastTone) ) {
+        throw Exception("The tone you've provided does not exist (yet) or isn't a valid tone")
     }
-    if((generateTuneMap()[firstTone.first()]!!.absoluteValue > generateTuneMap()[lastTone.first()]!!.absoluteValue)) {
-        throw Exception("The first tone can't be higher than the last")
-    }
-    if(firstTone.last() > lastTone.last()) {
-        throw Exception("Please check your octaves")
+    if((generateTuneMap()[firstTone]!!.absoluteValue >= generateTuneMap()[lastTone]!!.absoluteValue)) {
+        throw Exception("The first tone can't be higher or the same as the last one")
     }
     return true;
 }
-
 
 data class InstrumentAmbitus(val firstTone: String, val lastTone: String) {
     val generatedAmbitus : String;
