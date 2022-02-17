e0ee50af3944f5042537b7f221c64c2ada2784c7
liza-kl <hello@li54.dev>
2022-02-17 21:36:28 +0100

    :sparkles: Added Repository Interface

---
+++ domain/src/main/kotlin/de/dhbw/ka/domain/repositories/Repository.kt
@@ -0,0 +1,11 @@
+package de.dhbw.ka.domain.repositories
+
+interface Repository {
+    interface Repository<Entity : Any, ID : Any> {
+        suspend fun findById(id: ID): Entity?
+        suspend fun findAll(): List<Entity>
+        suspend fun create(entity: Entity): Entity
+        suspend fun update(entity: Entity): Entity
+        suspend fun delete(id: ID)
+    }
+}
