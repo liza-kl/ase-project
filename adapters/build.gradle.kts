a0d51c0f2e3c655038617f6ddea60f8bd4e88a5a
liza-kl <hello@li54.dev>
2022-02-17 08:30:50 +0100

    :sparkles: Added Sample DTO Objects

---
+++ adapters/build.gradle.kts
@@ -0,0 +1,6 @@
+val ktorVersion: String by project
+
+dependencies {
+    implementation(projects.useCases)
+    implementation(projects.domain)
+}
---
+++ adapters/src/main/kotlin/MemberDTO.kt
@@ -0,0 +1,4 @@
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MemberDTO(val blub: String)
