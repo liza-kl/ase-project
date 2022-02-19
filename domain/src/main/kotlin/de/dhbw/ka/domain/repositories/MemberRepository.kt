2da7547938da5bb31f9d152ab430ea550b527d8a
liza-kl <hello@li54.dev>
2022-02-19 19:03:34 +0100

    :sparkles: MemberRepo

--- domain/src/main/kotlin/de/dhbw/ka/domain/repositories/MemberRepository.kt
+++ domain/src/main/kotlin/de/dhbw/ka/domain/repositories/MemberRepository.kt
@@ -3,11 +3,8 @@ package de.dhbw.ka.domain.repositories
 import de.dhbw.ka.domain.entities.Member
 
 interface MemberRepository {
-
-    suspend fun getMembersOfMusicSociety() : List<Member>
-
-    suspend fun addMember(member: Member)
-
-    suspend fun deleteMember(memberId: Int)
-
+    fun create(input: Member)
+    fun findById(id: Int): Member?
+    fun update(input: Member)
+    fun findAll(): List<Member>
 }
