dfe30bd52a6c6edb807c18208669b4c2ecdbddc7
liza-kl <hello@li54.dev>
2022-02-19 18:34:56 +0100

    :basket: deleted noise

--- use-cases/src/main/kotlin/de/dhbw/ka/services/GetMembersUseCase.kt
+++
@@ -1,7 +0,0 @@
-package de.dhbw.ka.services
-
-import de.dhbw.ka.domain.repositories.MemberRepository
-
-public class GetMembersUseCase(private val membersRepository: MemberRepository) {
-    suspend operator fun invoke()  = membersRepository.getMembersOfMusicSociety()
-}
