267e9d383c0993700ccb9458e771a3ace8c8c766
elisabeth.kletsko <ekletsko@w-commerce.de>
2022-03-10 14:00:49 +0100

    :sparkles: Borrow Instrument Use Case

--- use-cases/src/main/kotlin/de/dhbw/ka/lentinstruments/BorrowInstrument.kt
+++ use-cases/src/main/kotlin/de/dhbw/ka/lentinstruments/BorrowInstrument.kt
@@ -1,15 +1,22 @@
 package de.dhbw.ka.lentinstruments
 
-import de.dhbw.ka.domain.entities.Instrument
-import de.dhbw.ka.domain.entities.Member
 import de.dhbw.ka.domain.repository.LentInstrumentRepository
+import de.dhbw.ka.domain.repository.MemberRepository
+import de.dhbw.ka.domain.valueobjects.InstrumentIdentification
 import de.dhbw.ka.domain.valueobjects.MemberStatus
 
-class BorrowInstrument(private val lentInstrumentRepository : LentInstrumentRepository) {
-    fun execute(memberData: Member, instrumentData: Instrument) : Boolean {
-        if(memberData.memberStatus != MemberStatus("ACTIVE")) {
+class BorrowInstrument(
+    private val lentInstrumentRepository: LentInstrumentRepository,
+    private val memberRepository: MemberRepository
+) {
+    fun execute(memberId: Int, instrumentData: InstrumentIdentification): Boolean {
+        val findMember = memberRepository.findById(id = memberId)
+
+        if (
+            findMember?.memberStatus != MemberStatus("ACTIVE")
+        ) {
             throw IllegalArgumentException("Member must be ACTIVE to be able to borrow an Instrument")
         }
-        return lentInstrumentRepository.borrowInstrument(memberData.memberId, instrumentData)
+        return lentInstrumentRepository.borrowInstrument(memberId, instrumentData)
     }
 }
