3d45ef6611a41211eb7344aa11713b5b7b85d4b9
elisabeth.kletsko <ekletsko@w-commerce.de>
2022-03-13 21:14:01 +0100

    :bug: Changed the method so that an Exception is thrown if it's not possible to create an instrument

--- use-cases/src/main/kotlin/de/dhbw/ka/instruments/CheckIfInstrumentExists.kt
+++ use-cases/src/main/kotlin/de/dhbw/ka/instruments/CheckIfInstrumentExists.kt
@@ -6,7 +6,6 @@ import de.dhbw.ka.domain.valueobjects.InstrumentIdentification
 class CheckIfInstrumentExists(private val instrumentRepository: InstrumentRepository) {
     fun execute(instrumentToCheck: InstrumentIdentification) : Boolean {
         if (instrumentRepository.checkIfInstrumentExists(instrumentToCheck)) {
-           // throw Exception("Instrument already exists!")
             return true
         }
         return false
--- use-cases/src/main/kotlin/de/dhbw/ka/instruments/CreateInstrument.kt
+++ use-cases/src/main/kotlin/de/dhbw/ka/instruments/CreateInstrument.kt
@@ -5,9 +5,9 @@ import de.dhbw.ka.domain.repository.InstrumentRepository
 
 class CreateInstrument(private val instrumentRepository: InstrumentRepository) {
     fun execute(newInstrumentData: Instrument): Boolean {
-        if(!CheckIfInstrumentExists(instrumentRepository).execute(newInstrumentData.instrumentIdentification)) {
-            return instrumentRepository.createInstrument(instrumentData = newInstrumentData)
+        if(CheckIfInstrumentExists(instrumentRepository).execute(newInstrumentData.instrumentIdentification)) {
+            throw Exception("Instrument already exists")
         }
-        return false
+        return instrumentRepository.createInstrument(instrumentData = newInstrumentData)
     }
 }
