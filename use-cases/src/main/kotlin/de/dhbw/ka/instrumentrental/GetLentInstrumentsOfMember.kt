a95a9e722e61a2295997f7859eff67db159641cf
liza-kl <hello@li54.dev>
2022-03-23 14:51:34 +0100

    :recycle: Deleted dead / unused code

--- use-cases/src/main/kotlin/de/dhbw/ka/instrumentrental/GetLentInstrumentsOfMember.kt
+++
@@ -1,9 +0,0 @@
-package de.dhbw.ka.instrumentrental
-
-import de.dhbw.ka.domain.repository.InstrumentRentalEntryRepository
-
-class GetLentInstrumentsOfMember(private val instrumentRentalEntryRepository: InstrumentRentalEntryRepository) {
-    fun execute(memberId : Int) {
-
-    }
-}
--- use-cases/src/main/kotlin/de/dhbw/ka/members/CheckIfMemberExists.kt
+++
@@ -1,9 +0,0 @@
-package de.dhbw.ka.members
-
-import de.dhbw.ka.domain.repository.MemberRepository
-
-class CheckIfMemberExists(private val memberRepository: MemberRepository) {
-    fun execute(memberId: Int): Boolean {
-        return memberRepository.findById(memberId) != null
-    }
-}
