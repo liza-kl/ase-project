595611d828f645973734bf9a4d90098252bd9ca7
liza-kl <hello@li54.dev>
2022-03-23 13:09:54 +0100

    :white_check_mark: Wrote Test for Rental Instrument with a mock

---
+++ plugins/src/test/kotlin/de/dhbw/ka/controllers/RentalInstrumentControllerKtTest.kt
@@ -0,0 +1,37 @@
+package de.dhbw.ka.controllers
+
+import de.dhbw.ka.domain.aggregates.RentalInstrument
+import de.dhbw.ka.domain.repository.InstrumentRepository
+import de.dhbw.ka.domain.repository.RentalInstrumentRepository
+import de.dhbw.ka.domain.valueobjects.InstrumentIdentification
+import de.dhbw.ka.rentalinstruments.CreateRentalInstrument
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.string.startWith
+import io.mockk.mockk
+
+/**
+ * Used Testing Library: kotest.io
+ * Used Mocking Library: mockk.io
+ */
+
+/**
+ * Test with a mock
+ */
+class RentalInstrumentControllerKtTest : FunSpec({
+
+    val rentalInstrumentRepository = mockk<RentalInstrumentRepository>(relaxed=true)
+    val instrumentRepository = mockk<InstrumentRepository>(relaxed=true)
+    val useCase = CreateRentalInstrument(rentalInstrumentRepository, instrumentRepository)
+    val rentalInstrument = RentalInstrument(
+        InstrumentIdentification("Yamaha", "YHR-567D", "French Horn"),
+        3
+    )
+    test("Should not create a Rental Instrument") {
+        val exception = shouldThrow<Exception> {
+            useCase.execute(rentalInstrument)         }
+
+        exception.message should startWith("Instrument must be present in the Instrument database")
+    }
+})
