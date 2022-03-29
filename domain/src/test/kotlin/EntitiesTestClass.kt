47429e2c73e7dd08bb9c081a7d756997e0ae1e80
liza-kl <hello@li54.dev>
2022-03-29 08:45:47 +0200

    :white_check_mark: Wrote 2 other test cases

--- domain/src/test/kotlin/EntitiesTestClass.kt
+++ domain/src/test/kotlin/EntitiesTestClass.kt
@@ -1,5 +1,9 @@
 
+import de.dhbw.ka.domain.aggregates.InstrumentRentalEntry
+import de.dhbw.ka.domain.entities.Instrument
 import de.dhbw.ka.domain.entities.Member
+import de.dhbw.ka.domain.valueobjects.InstrumentCategory
+import de.dhbw.ka.domain.valueobjects.InstrumentIdentification
 import de.dhbw.ka.domain.valueobjects.MemberName
 import de.dhbw.ka.domain.valueobjects.MemberStatus
 import io.kotest.core.spec.style.FunSpec
@@ -12,5 +16,22 @@ class EntitiesTestClass: FunSpec({
         createdMember.memberName.firstName shouldBe "Max"
         createdMember.memberName.lastName shouldBe "Mustermann"
         createdMember.memberStatus.status shouldBe "ACTIVE"
+    }
+
+    test("Should create an Instrument") {
+        val createdInstrument = Instrument(InstrumentIdentification("Thomann","Little Bear","Bb-Tuba"),
+            InstrumentCategory("BRASS"))
+        createdInstrument.instrumentIdentification.instrumentType shouldBe "Bb-Tuba"
+        createdInstrument.instrumentIdentification.instrumentSerialNumber shouldBe "Little Bear"
+        createdInstrument.instrumentIdentification.instrumentManufacturer shouldBe "Thomann"
+    }
+
+    test("Should create a InstrumentRentalEntry") {
+        val createdInstrumentRental = InstrumentRentalEntry(1,2,
+            InstrumentIdentification("Yamaha",
+            "YHR-752","French Horn"))
+        createdInstrumentRental.rentalEntryId shouldBe 1
+        createdInstrumentRental.memberId shouldBe 2
+        createdInstrumentRental.instrumentIdentification shouldBe InstrumentIdentification(instrumentManufacturer="Yamaha", instrumentSerialNumber="YHR-752", instrumentType="French Horn")
     }
 })
