b5b33c5d85b4e17dce919a7527692c67b6869e28
liza-kl <hello@li54.dev>
2022-04-05 15:51:58 +0200

    :recycle: Added Exception Maessages for better clarity

--- domain/src/main/kotlin/de/dhbw/ka/domain/services/RentalRequestService.kt
+++ domain/src/main/kotlin/de/dhbw/ka/domain/services/RentalRequestService.kt
@@ -1,5 +1,7 @@
 package de.dhbw.ka.domain.services
 
+import de.dhbw.ka.domain.exceptions.WrongMemberStatus
+import de.dhbw.ka.domain.exceptions.WrongQuantity
 import de.dhbw.ka.domain.specifications.EnoughInstrumentsAvailableRule
 import de.dhbw.ka.domain.specifications.MemberIsActiveRule
 import de.dhbw.ka.domain.valueobjects.RentalRequest
@@ -7,6 +9,14 @@ import de.dhbw.ka.domain.valueobjects.RentalRequest
 class RentalRequestService {
     fun approve(rentalRequest: RentalRequest) : Boolean {
         val listOfRulesToCheck = listOf(MemberIsActiveRule, EnoughInstrumentsAvailableRule)
-        return listOfRulesToCheck.all { it.isSatisfiedBy(rentalRequest) }
+         listOfRulesToCheck.map {
+                if(!it.isSatisfiedBy(rentalRequest)) {
+                    when(it) {
+                        MemberIsActiveRule -> throw WrongMemberStatus().throwsException()
+                        EnoughInstrumentsAvailableRule -> throw WrongQuantity().throwsException()
+                    }
+                }
+        }
+        return true
     }
 }
